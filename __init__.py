a7cf7dc25a2c09d5a54a609468d63ccba45fef64
katiaa02 <93008935+katiaa02@users.noreply.github.com>
2025-05-21 10:18:50 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask import json
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)   #1                                                                                                               
+app = Flask(__name__)   #12                                                                                                               
                                                                                                                                        
 @app.route('/')
 def hello_world():
